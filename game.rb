66ea731c5907c6897e79aab43f64b4aed84b817e
Erik Ivares <erik.ivares@elev.it-gymnasiet.se>
2012-12-07 16:32:05 +0100

    added alot of think as meteor ann collisions

--- game.rb
+++ game.rb
@@ -5,12 +5,22 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		Level.create
+		5.times {Meteor.create}
 		Player.create
 	end
+	def update
+		super
+		Laser.each_bounding_circle_collision(Meteor) do |laser, target|
+      		laser.destroy
+      		target.destroy
+      	end
+    end
+
 end
 
 class Player < Chingu::GameObject
-
+	has_traits :velocity, :timer
 	#meta construktur
 	def setup
 		@x = 400
@@ -23,38 +33,83 @@ class Player < Chingu::GameObject
 			holding_down: :down,
 			space: :fire
 		}
-
+		@speed = 10
+		@angle = 0
 	end
 
+	def update
+		self.velocity_x *= 0.95
+		self.velocity_y *= 0.95
+
+		@x %= 800
+		@y %= 600
+	end
+
+
 	def left
-		@x -= 5
+		@angle -= 4.5
 	end
 
 	def right
-		@x += 5
+		@angle += 4.5
 	end
 
 	def up
-		@y -= 5
+		self.velocity_y += Gosu::offset_y(@angle, 0.5) 
+		self.velocity_x += Gosu::offset_x(@angle, 0.5) 
 	end
 
 	def down
-		@y += 5
+		self.velocity_y -= Gosu::offset_y(@angle, 0.5) 
+		self.velocity_x -= Gosu::offset_x(@angle, 0.5) 
 	end
 
 	def fire
-		Laser.create(x: @x, y: @y)
+		Laser.create(x: self.x, y: self.y, angle: self.angle)
 	end
-
 end
 
 class Laser < Chingu::GameObject
 
-	has_traits :velocity
+	has_traits :velocity, :collision_detection, :bounding_circle, :timer
 
 	def setup
 		@image = Gosu::Image["shot.png"]
-		self.velocity_y = -10
+		self.velocity_y += Gosu::offset_y(@angle, 20)
+		self.velocity_x += Gosu::offset_x(@angle, 20) 
+		after(1001) {self.destroy}
+	end
+	def update
+		@x %= 800
+		@y %= 600
+	end
+
+
+end
+
+class Level < Chingu::GameObject
+	def setup
+		@x = 400
+		@y = 300
+		@image = Gosu::Image["space.png"]
+	end
+
+end
+
+class Meteor < Chingu::GameObject
+
+	has_traits :collision_detection, :bounding_circle, :velocity
+
+	def setup
+		@image = Gosu::Image["meteor.png"]
+		@angle = rand(359)
+		self.velocity_y += Gosu::offset_y(@angle, rand(1...6))
+		self.velocity_x += Gosu::offset_x(@angle, rand(1...6)) 
+	end
+
+	def update
+		@x %= 800
+		@y %= 600
 	end
 
 
