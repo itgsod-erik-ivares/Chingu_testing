0bb9e43eb9bad3a586af9b2ebc883787f694b6a0
Erik Ivares <erik.ivares@elev.it-gymnasiet.se>
2012-12-10 21:50:24 +0100

    added more to animation but it doss't work....
    Also added collision with player but only results in crach when it
    happens, which of coarse is partly what i whant but it loocks bad XD.
    Will continue to work on animation next time.
    //first time waorking from home, Erik Ivares XD

--- game.rb
+++ game.rb
@@ -1,10 +1,16 @@
+require 'rubygems' rescue nil
+$LOAD_PATH.unshift File.join(File.expand_path(__FILE__), "..", "..", "lib")
 require 'chingu'
+include Gosu
+include Chingu
 
 class Game < Chingu::Window
 
 	def initialize
 		super
-		self.input = {esc: :exit}
+		self.input = {esc: :exit}	
+			self.caption = "Astroid game! Made myself!"
+		#retrofy
 		Level.create
 		5.times {Meteor.create}
 		Player.create
@@ -15,45 +21,47 @@ class Game < Chingu::Window
        		laser.destroy
        		target.destroy
        	end
+       	Meteor.each_bounding_circle_collision(Player) do |meteor, player|
+       		player.destroy
+       		meteror.destroy
+       	end
      end
 
 end
 
 class Player < Chingu::GameObject
-	has_traits :velocity, :timer
+	has_traits :velocity, :collision_detection, :bounding_circle, :timer
+
 	#meta construktur
 	def setup
+		super
 		@x = 400
 		@y = 300
 		@image = Gosu::Image["Ship.png"]
-		self.input = {
-			holding_left: :left,
-			holding_right: :right,
-			holding_up: :up,
-			holding_down: :down,
-			space: :fire
-		}
+		 self.input = {
+		 	holding_left: :left,
+		 	holding_right: :right,
+		 	holding_up: :up,
+		 	holding_down: :down,
+		 	space: :fire
+		 }
 		@speed = 10
 		@angle = 0
 		@animation = Chingu::Animation.new(:file => "flame_48x48.png")
-		@animation.frame_names = { still: => 0..1, :up => 2..5, :fire => 6..7}
+		@animation.frame_names = { :still =>0..1, :up =>2..5, :fire =>6..7}
+# :still =>0..1, :fire =>6..7, :up =>2..5
+		@frame_name = :still
+		@last_x, @last_y = @x, @y
 	end
-
-	 def update
-	 	self.velocity_x *= 0.95
-	 	self.velocity_y *= 0.95
-
-	 	@x %= 800
-	 	@y %= 600
-	 end
-
 
 	def left
 		@angle -= 4.5
+		@frame_name = :up
 	end
 
 	def right
 		@angle += 4.5
+		@frame_name = :up
 	end
 
 	def up
@@ -69,6 +77,18 @@ class Player < Chingu::GameObject
 
 	def fire
 		Laser.create(x: @x, y: @y, angle: @angle)
+		@frame_name = :fire
+	end
+
+	def update
+	 	@frame_name = :still if @x == @last_x && @y == @last_y
+
+	 	@last_x, @last_y = @x, @y
+	 	self.velocity_x *= 0.95
+	 	self.velocity_y *= 0.95
+
+	 	@x %= 800
+	 	@y %= 600
 	end
 end
 
@@ -82,11 +102,9 @@ class Laser < Chingu::GameObject
 		@image = Gosu::Image["shot.png"]
 		self.velocity_y += Gosu::offset_y(@angle, 20)
 		self.velocity_x += Gosu::offset_x(@angle, 20) 
-		after(1001) {self.destroy}
+		after(750) {self.destroy}
 	end
 	def update
-		@x %= 800
-		@y %= 600
 	end
 end
 
